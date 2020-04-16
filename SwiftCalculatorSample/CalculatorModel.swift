48c0358cd346d75ad72f808bc3ac94d77f16480c
Matsuo Takuma <matku10j4@icloud.com>
2020-04-16 18:19:11 +0900

    4/16

--- SwiftCalculatorSample/CalculatorModel.swift
+++ SwiftCalculatorSample/CalculatorModel.swift
@@ -11,4 +11,5 @@ class CalculatorModel:UIViewController,UITabBarControllerDelegate{
             ButtonArray.append(Button)
         }
     }
+    
 }
--- SwiftCalculatorSample/ViewController2.swift
+++ SwiftCalculatorSample/ViewController2.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class ViewController2: UIViewController {
     var CAL = CalculatorModel()
+    var VC = ViewController()
+    var CalculatorLabel = UILabel()
+    
+    
     override func viewDidLoad() {
         CAL.BunttonModel()
         let ButtonArray = CAL.ButtonArray
@@ -9,17 +13,15 @@ class ViewController2: UIViewController {
         let frameSizeWidth = view.frame.size.width
         let tabBarFrameSizeHeight = tabBarController?.tabBar.frame.size.height
         let ButtonSize = 1 / 5 * frameSizeWidth
-        
-        
-        
-        
         let y = frameSizeHeight - tabBarFrameSizeHeight! - ButtonSize - (frameSizeWidth / 25)
         let x = frameSizeWidth / 25
+        
+        
         for i in 0..<20 {
             func ButtonInit(Number:Int,xSide:CGFloat,ySide:CGFloat){
                 ButtonArray[Number].frame = .init(x: xSide, y:ySide, width: ButtonSize, height: ButtonSize)
             }
-            switch i {
+            switch i {//配置決め
             case 0...3:
                 ButtonInit(Number: i, xSide: CGFloat(i + 1) * x + CGFloat(i) * ButtonSize, ySide: y)
             case 4...7:
@@ -31,9 +33,17 @@ class ViewController2: UIViewController {
             case 16...19:
                 ButtonInit(Number: i, xSide: CGFloat(i % 16 + 1) * x + CGFloat(i % 16) * ButtonSize, ySide: y - 4 * (x + ButtonSize))
             default:
-                print("該当するcaseなし")
+                print("該当するcaseなし\(i)")
             }
-            switch i {
+            
+            CalculatorLabel.frame = .init(x: x, y: (y - 4 * (x + ButtonSize)) / 2, width: frameSizeWidth - CGFloat(2) * x, height: (y - 4 * (x + ButtonSize)) * 0.4)
+            CalculatorLabel.font = .boldSystemFont(ofSize: ((y - 4 * (x + ButtonSize)) * 0.4) / 3)
+            CalculatorLabel.backgroundColor = .none
+            CalculatorLabel.numberOfLines = 2
+            CalculatorLabel.textAlignment = .right
+            CalculatorLabel.text = "テストです"
+            
+            switch i {//文字盤
             case 0:
                 ButtonArray[i].setTitle("\(i)", for: .normal)
             case 4...6:
@@ -61,7 +71,7 @@ class ViewController2: UIViewController {
             case 15:
                  ButtonArray[i].setTitle("÷", for: .normal)
             default:
-                print("該当するcaseなし")
+                print("該当するcaseなし\(i)")
             }
         }
         
@@ -71,9 +81,27 @@ class ViewController2: UIViewController {
         super.viewDidLoad()
         for i in 0..<20 {
             view.addSubview(ButtonArray[i])
-            print(i)
+             ButtonArray[i].addTarget(self, action: #selector(self.tapButton(_ :)), for: .touchUpInside)
         }
+        view.addSubview(CalculatorLabel)
         print("開始しました")
+    }
+    @objc func tapButton(_ sender: UIButton){
+        var provisionalNumber = VC.provisionalNumber
+        var sample = VC.sample
+        var formula = VC.formula//1つ手前までの計算式の格納変数。Clearのために使う
+        var OperationSymbol = VC.OperationSymbol //演算記号の条件に使う
+        switch sender.tag {
+        case 1:
+            if  provisionalNumber != ""{//最初に0は使えないようにする
+            CalculatorLabel.text = "\(CalculatorLabel.text!)" + "0"
+                provisionalNumber += "0"
+            }
+            print(sender.tag)
+        default:
+            print(sender.tag)
+        }
+        
     }
 }
     
