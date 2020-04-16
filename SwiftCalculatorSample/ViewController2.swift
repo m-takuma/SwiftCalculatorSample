f5a31c13aa63a9dab726704c28e9dcb5b2a5b64d
Matsuo Takuma <matku10j4@icloud.com>
2020-04-16 19:44:25 +0900

    4/16

--- SwiftCalculatorSample/ViewController2.swift
+++ SwiftCalculatorSample/ViewController2.swift
@@ -4,6 +4,10 @@ class ViewController2: UIViewController {
     var CAL = CalculatorModel()
     var VC = ViewController()
     var CalculatorLabel = UILabel()
+    var provisionalNumber = String()//現在の数字の格納変数計算時にInt型にする
+    var sample:Double! = nil//計算結果の格納変数
+    var formula = String()//1つ手前までの計算式の格納変数。Clearのために使う
+    var OperationSymbol = String()//演算記号の条件に使う
     
     
     override func viewDidLoad() {
@@ -38,10 +42,10 @@ class ViewController2: UIViewController {
             
             CalculatorLabel.frame = .init(x: x, y: (y - 4 * (x + ButtonSize)) / 2, width: frameSizeWidth - CGFloat(2) * x, height: (y - 4 * (x + ButtonSize)) * 0.4)
             CalculatorLabel.font = .boldSystemFont(ofSize: ((y - 4 * (x + ButtonSize)) * 0.4) / 3)
-            CalculatorLabel.backgroundColor = .none
+            CalculatorLabel.backgroundColor = .brown
             CalculatorLabel.numberOfLines = 2
             CalculatorLabel.textAlignment = .right
-            CalculatorLabel.text = "テストです"
+            CalculatorLabel.text = ""
             
             switch i {//文字盤
             case 0:
@@ -64,11 +68,11 @@ class ViewController2: UIViewController {
                 ButtonArray[i].setTitle("=", for: .normal)
             case 7:
                  ButtonArray[i].setTitle("+", for: .normal)
-            case 9:
-                 ButtonArray[i].setTitle("-", for: .normal)
             case 11:
-                 ButtonArray[i].setTitle("×", for: .normal)
+                 ButtonArray[i].setTitle("-", for: .normal)
             case 15:
+                 ButtonArray[i].setTitle("×", for: .normal)
+            case 19:
                  ButtonArray[i].setTitle("÷", for: .normal)
             default:
                 print("該当するcaseなし\(i)")
@@ -87,21 +91,72 @@ class ViewController2: UIViewController {
         print("開始しました")
     }
     @objc func tapButton(_ sender: UIButton){
-        var provisionalNumber = VC.provisionalNumber
-        var sample = VC.sample
-        var formula = VC.formula//1つ手前までの計算式の格納変数。Clearのために使う
-        var OperationSymbol = VC.OperationSymbol //演算記号の条件に使う
         switch sender.tag {
         case 1:
             if  provisionalNumber != ""{//最初に0は使えないようにする
             CalculatorLabel.text = "\(CalculatorLabel.text!)" + "0"
                 provisionalNumber += "0"
             }
-            print(sender.tag)
+        case 5...7:
+            let Number = sender.tag - 4
+            CalculatorLabel.text = "\(CalculatorLabel.text!)" + "\(Number)"
+                       provisionalNumber += "\(Number)"
+        case 9...11:
+            let Number = sender.tag - 5
+            CalculatorLabel.text = "\(CalculatorLabel.text!)" + "\(Number)"
+                       provisionalNumber += "\(Number)"
+        case 13...15:
+            let Number = sender.tag - 6
+            CalculatorLabel.text = "\(CalculatorLabel.text!)" + "\(Number)"
+                       provisionalNumber += "\(Number)"
+        case 2://clear
+            CalculatorLabel.text = formula//保存しておいた計算式を取り出す
+            provisionalNumber = ""//リセットする
+        case 3:
+            print("AllClear")
+        case 4:
+            print("Equal")
+        case 8,12,16,20:
+            if provisionalNumber != "" {
+                if sample == nil{
+                    sample = Double(provisionalNumber)!
+                }else{
+                    switch OperationSymbol {
+                    case "Plus":
+                        sample = sample + Double(provisionalNumber)!
+                    case "Minus":
+                        sample = sample - Double(provisionalNumber)!
+                    case "Multiplication":
+                        sample = sample * Double(provisionalNumber)!
+                    case "Division":
+                        sample = sample / Double(provisionalNumber)!
+                    default:
+                        print("caseがありません")
+                    }
+                }
+                CalculatorLabel.text = String(sample)
+                switch sender.tag {
+                case 8:
+                    OperationSymbol = "Plus"
+                    CalculatorLabel.text = "\(CalculatorLabel.text!)" + "+"
+                case 12:
+                    OperationSymbol = "Minus"
+                    CalculatorLabel.text = "\(CalculatorLabel.text!)" + "-"
+                case 16:
+                    OperationSymbol = "Multiplication"
+                    CalculatorLabel.text = "\(CalculatorLabel.text!)" + "×"
+                case 20:
+                    OperationSymbol = "Division"
+                    CalculatorLabel.text = "\(CalculatorLabel.text!)" + "÷"
+                default:
+                    print("エラーです")
+                }
+                provisionalNumber = ""
+                formula = CalculatorLabel.text!
+            }
         default:
-            print(sender.tag)
+            print("caseがありません")
         }
-        
     }
 }
     
